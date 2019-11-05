bc85f6cf64d27bd7d4ef7529174ec6cf9266e2c3
hayamishun <shun.883@goole.com>
2019-11-05 11:22:55 +0900

    課題A-4完成

--- kadai01.py
+++ kadai01.py
@@ -3,5 +3,10 @@ users = ['Bob', 'Tom', 'Ken']
 
 # A-2
 int_number = [1, 2, 3, 4, 5]
+
 # A-3
 kazuma_info = ['Kazuma', 'Takahashi', 35]
+
+# A-4
+members = ['Kazuma', 'Makoto', 'Ohira']
+print(members[0], members[1])
